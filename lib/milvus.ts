8f7b09351166880b6ad763abf7f77be2d7d884ad
wanteatfruit <feasiblechart@163.com>
2023-05-22 13:03:00 +0800

    bug fix

--- lib/milvus.ts
+++ lib/milvus.ts
@@ -13,14 +13,16 @@ console.info("Connecting to milvus");
 const { uri, user, password, secure } = mconfig;
 if(process.env.NODE_ENV=='production'){
     milvus = new MilvusClient("118.195.181.111:19530");
+    console.log("");
     // milvus = null;
 }else{
     if(!global.milvus){
         console.log("No milvus available, creating new milvus.");
         // global.milvus = new MilvusClient(uri, true, user, password);
-        global.milvus = new MilvusClient("localhost:19530")
+        global.milvus = new MilvusClient("118.195.181.111:19530")
     }
     milvus = global.milvus;
+
 }
 console.info("Success");
 export default milvus;
